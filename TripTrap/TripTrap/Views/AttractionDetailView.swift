d914f2c88d1c43fc71a8d8997d99b91c7fe4ecb7
shinra-electric <50119606+shinra-electric@users.noreply.github.com>
2022-12-15 11:10:34 +0100

    Pin name in AttractionView

--- TripTrap/TripTrap/Views/AttractionDetailView.swift
+++ TripTrap/TripTrap/Views/AttractionDetailView.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 
 struct AttractionDetailView: View {
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    
     var attractions: Feature
-
-//    let exampleImage: String = "https://http.cat/404"
+    
+    //    let exampleImage: String = "https://http.cat/404"
     
     var body: some View {
         GeometryReader { proxy in
@@ -23,32 +25,41 @@ struct AttractionDetailView: View {
                     .frame(height: size.height / 2)
                     .frame(maxWidth: size.width)
                 
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text(attractions.properties.name)
-//                        Text("Guinness Storehouse")
-                            .font(.title)
-                            .foregroundColor(.primary)
+                LazyVStack(alignment: .leading, pinnedViews: .sectionHeaders) {
+                    Section(header: HeaderView(name: attractions.properties.name)) {
+
+                        HStack {
+                            Text("Brewery")
+                            Spacer()
+                        }
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                        
+                        Divider()
+                        
+                        Text("About\n")
+                            .font(.title2)
+                        Text(attractions.properties.wikidata)
+                        
+                        Text("""
+                                    Guinness Storehouse is a tourist attraction at St. James's Gate Brewery in Dublin, Ireland. Since opening in 2000, it has received over twenty million visitors.
+                                    
+                                    The Storehouse covers seven floors surrounding a glass atrium shaped in the form of a pint of Guinness.[5] The ground floor introduces the beer's four ingredients (water, barley, hops and yeast), and the brewery's founder, Arthur Guinness. Other floors feature the history of Guinness advertising and include an interactive exhibit on responsible drinking. The seventh floor houses the Gravity Bar with views of Dublin and where visitors may drink a pint of Guinness included in the price of admission.
+                                    
+                                    """)
+                        
+                        Text("""
+                                    Guinness Storehouse is a tourist attraction at St. James's Gate Brewery in Dublin, Ireland. Since opening in 2000, it has received over twenty million visitors.
+                                    
+                                    The Storehouse covers seven floors surrounding a glass atrium shaped in the form of a pint of Guinness.[5] The ground floor introduces the beer's four ingredients (water, barley, hops and yeast), and the brewery's founder, Arthur Guinness. Other floors feature the history of Guinness advertising and include an interactive exhibit on responsible drinking. The seventh floor houses the Gravity Bar with views of Dublin and where visitors may drink a pint of Guinness included in the price of admission.
+                                    
+                                    """)
+                        
+                        
                     }
+//                    .headerProminence(.increased)
                     
-                    HStack {
-                        Text("Brewery")
-                        Spacer()
-                    }
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
                     
-                    Divider()
-                    
-                    Text("About\n")
-                        .font(.title2)
-                    Text(attractions.properties.wikidata)
-//                    Text("""
-//Guinness Storehouse is a tourist attraction at St. James's Gate Brewery in Dublin, Ireland. Since opening in 2000, it has received over twenty million visitors.
-//
-//The Storehouse covers seven floors surrounding a glass atrium shaped in the form of a pint of Guinness.[5] The ground floor introduces the beer's four ingredients (water, barley, hops and yeast), and the brewery's founder, Arthur Guinness. Other floors feature the history of Guinness advertising and include an interactive exhibit on responsible drinking. The seventh floor houses the Gravity Bar with views of Dublin and where visitors may drink a pint of Guinness included in the price of admission.
-//
-//""")
                 }
                 .padding()
             }
@@ -75,5 +86,23 @@ struct image: View {
 struct AttractionDetailView_Previews: PreviewProvider {
     static var previews: some View {
         AttractionDetailView(attractions: Attractions.exampleFeature)
+    }
+}
+
+struct HeaderView: View {
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    var name: String
+    var body: some View {
+        HStack {
+            
+            Text(name)
+                .font(.title2)
+                .foregroundColor(.primary)
+            Spacer()
+        }
+        .padding(.vertical)
+        .frame(maxWidth: .infinity)
+        .background(colorScheme == .light ? Color.white : Color.black)
+        
     }
 }
