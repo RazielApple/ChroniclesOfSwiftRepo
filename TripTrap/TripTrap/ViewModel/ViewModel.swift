bfe47b407d1539925617daa37dccd97a9b8c0fb1
shinra-electric <50119606+shinra-electric@users.noreply.github.com>
2022-12-18 16:22:19 +0100

    VM: Use variables in the openTripMap API URL

--- TripTrap/TripTrap/ViewModel/ViewModel.swift
+++ TripTrap/TripTrap/ViewModel/ViewModel.swift
@@ -14,34 +14,38 @@ class ViewModel: ObservableObject {
     
     @MainActor
     func fetch(searchText: String) async throws {
-            let postData = NSData(data: "".data(using: String.Encoding.utf8)!)
-
-            let request = NSMutableURLRequest(url: NSURL(string: "https://api.opentripmap.com/0.1/en/places/autosuggest?name=\(searchText)&radius=10000&lon=14.26811&lat=40.85216&apikey=5ae2e3f221c38a28845f05b66c319f2c844159541f0810838b11d4db")! as URL,
-                                                cachePolicy: .useProtocolCachePolicy,
-                                                timeoutInterval: 10.0)
-            request.httpMethod = "GET"
-            request.httpBody = postData as Data
-
-            let session = URLSession.shared
-            let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
-              if (error != nil) {
+        let postData = NSData(data: "".data(using: String.Encoding.utf8)!)
+        let openTripMapAPIKey = "5ae2e3f221c38a28845f05b66c319f2c844159541f0810838b11d4db"
+        let radius = "10000"
+        let longitude = "14.26811"
+        let latitude = "40.85216"
+        
+        let request = NSMutableURLRequest(url: NSURL(string: "https://api.opentripmap.com/0.1/en/places/autosuggest?name=\(searchText)&radius=\(radius)&lon=\(longitude)&lat=\(latitude)&apikey=\(openTripMapAPIKey)")! as URL,
+                                          cachePolicy: .useProtocolCachePolicy,
+                                          timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.httpBody = postData as Data
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
                 print(error!)
-              } else {
+            } else {
                 let httpResponse = response as? HTTPURLResponse
-                  print(httpResponse!)
-              }
-                
-
-                let fetchedData = try? JSONDecoder().decode(Attractions.self, from: data!)
-                
-                if let featuresArray = fetchedData?.features {
-                    DispatchQueue.main.async {
-                        self.attractionsList = featuresArray
-                    }
+                print(httpResponse!)
+            }
+            
+            
+            let fetchedData = try? JSONDecoder().decode(Attractions.self, from: data!)
+            
+            if let featuresArray = fetchedData?.features {
+                DispatchQueue.main.async {
+                    self.attractionsList = featuresArray
                 }
-            })
-            dataTask.resume()
-        }
+            }
+        })
+        dataTask.resume()
+    }
     
     
     private func getMD5Hash(itemName: String) -> String {
